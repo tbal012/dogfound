6e9e16230d0efe125dbe8d2ab2cb246f7530a287
Todd Ballantyne <toddballantyne@hotmail.com>
2016-07-31 13:42:20 +1200

    Initial commit

---
+++ Other/Alpaca.Web/__DO_NOT_PUBLISH/_App_Code/Utilities.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+
+public static class Utilities
+{
+    public static bool isValidIP(string ip)
+    {
+        IPAddress ot;
+        bool valid = false;
+        if (string.IsNullOrEmpty(ip)){
+            valid = false;
+        }
+        else{
+            valid = IPAddress.TryParse(ip, out ot);
+        }
+        return valid;
+    }
+
+
+}
+
+public class Users
+{
+    public string Email { get; set; }
+    public string UserName { get; set; }
+    public string DisplayName { get; set; }
+    public bool isMapped { get; set; }
+}
